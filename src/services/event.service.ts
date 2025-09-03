e060a44b7e5de28b9da1910996f6ef539bb99fac
Angelo <sachetwd@gmail.com>
2025-09-03 13:33:47 -0300

    feat(event): add websocket broadcast for queue and player events
    Add WebSocketService integration to broadcast queue and player events to connected clients. This enables real-time updates for queue operations (player added/removed/moved) and player state changes (created/updated).

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,10 +1,14 @@
 import { EventEmitter } from 'events';
+import { WebSocketService } from './websocket.service';
 
 export class EventService {
   private emitter: EventEmitter;
+  private wsService: WebSocketService;
 
   constructor() {
     this.emitter = new EventEmitter();
+    this.wsService = WebSocketService.getInstance();
+    this.setupWebSocketBroadcasts();
   }
 
   emit(event: string, data?: any) {
@@ -17,6 +21,47 @@ export class EventService {
 
   off(event: string, listener: (data: any) => void) {
     this.emitter.off(event, listener);
+  }
+
+  private setupWebSocketBroadcasts() {
+    // Queue events
+    this.on('queue.playerAdded', (data) => {
+      this.wsService.broadcastQueueUpdate(data.simulatorId, data);
+    });
+
+    this.on('queue.playerRemoved', (data) => {
+      this.wsService.broadcastQueueUpdate(data.simulatorId, data);
+    });
+
+    this.on('queue.playerMoved', (data) => {
+      this.wsService.broadcastQueueUpdate(data.simulatorId, data);
+    });
+
+    // Timed queue events
+    this.on('timedQueue.started', (data) => {
+      this.wsService.broadcastTimedQueueUpdate(data.simulatorId, data);
+    });
+
+    this.on('timedQueue.playerActivated', (data) => {
+      this.wsService.broadcastTimedQueueUpdate(data.simulatorId, data);
+    });
+
+    this.on('timedQueue.playerConfirmed', (data) => {
+      this.wsService.broadcastTimedQueueUpdate(data.simulatorId, data);
+    });
+
+    this.on('timedQueue.playerCompleted', (data) => {
+      this.wsService.broadcastTimedQueueUpdate(data.simulatorId, data);
+    });
+
+    // Player events
+    this.on('player.created', (data) => {
+      this.wsService.broadcastPlayerUpdate(data);
+    });
+
+    this.on('player.updated', (data) => {
+      this.wsService.broadcastPlayerUpdate(data);
+    });
   }
 }
 
